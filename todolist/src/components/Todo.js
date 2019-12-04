99e4feca24c9ff431fd2a36b37a55e48d14290a4
Quevedo <jorge.a.quevedo@accenture.com>
2019-12-04 16:43:15 -0600

    Axios removed

--- todolist/src/components/Todo.js
+++ todolist/src/components/Todo.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 class Todo extends Component {
   state = {
@@ -8,17 +7,13 @@ class Todo extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://reacthooks-5dd63.firebaseio.com/todos.json")
-      .then(res => {
-        const todoData = res.data;
-        const todos = Object.keys(todoData).reduce((arr, key) => {
-          arr = [...arr, { id: key, name: todoData[key].name }];
-          return arr;
-        }, []);
-        this.setState({ todoName: "", todoList: todos });
-      })
-      .catch(err => console.log(err));
+    const todos = [
+      { id: "Task 10", name: "Task 1" },
+      { id: "Task 21", name: "Task 2" },
+      { id: "Task 32", name: "Task 3" }
+    ];
+
+    this.setState({ todoName: "", todoList: todos });
   }
 
   inputChangeHandler(event) {
@@ -28,32 +23,21 @@ class Todo extends Component {
   }
 
   todoAddHandler = () => {
-    axios
-      .post("https://reacthooks-5dd63.firebaseio.com/todos.json", {
-        name: this.state.todoName
-      })
-      .then(res => {
-        const todoItem = { id: res.data.name, name: this.state.todoName };
-        this.setState({
-          todoList: [...this.state.todoList, todoItem],
-          todoName: ""
-        });
-      })
-      .catch(err => console.log(err));
+    const todoItem = {
+      id: this.state.todoList.length + this.state.todoName,
+      name: this.state.todoName
+    };
+    this.setState({
+      todoList: [...this.state.todoList, todoItem],
+      todoName: ""
+    });
   };
 
   todoRemoveHandler = todoId => {
-    axios
-      .delete(`https://reacthooks-5dd63.firebaseio.com/todos/${todoId}.json`)
-      .then(() => {
-        let newState = [...this.state.todoList].filter(
-          todo => todo.id !== todoId
-        );
-        this.setState({
-          todoList: newState
-        });
-      })
-      .catch(err => console.log(err));
+    let newState = [...this.state.todoList].filter(todo => todo.id !== todoId);
+    this.setState({
+      todoList: newState
+    });
   };
 
   render() {
